5677bbb6f18fd36669df851ebd2a9633ae7cf6ab
BhabaniSankar-35 <biswalbhabanisankar@848gmail.com>
2026-02-14 22:29:17 -0800

    First Commit Ever

---
+++ src/com/Basic/SumOfNo.java
@@ -0,0 +1,15 @@
+package com.Basic;
+
+public class SumOfNo {
+
+	public static void main(String[] args) {
+		//Initializing two value
+       int num1=10;
+       int num2=20;
+       //adding 2 value and storing it in sum
+       int sum=num1+num2;
+       //Printing sum of values on output screen
+       System.out.println("Sum of num1 & num2 is: "+sum);
+	}
+
+}
