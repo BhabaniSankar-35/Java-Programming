1e0a1da86943878f7bb8564c5b7aaee89f0d4a39
BhabaniSankar-35 <biswalbhabanisankar@848gmail.com>
2026-03-04 03:25:54 -0800

    Adding new file

---
+++ src/com/Basic/A.java
@@ -0,0 +1,19 @@
+//It is not mandatory that the file name should match the class Name in which main() method is present.
+//public class name = File name.
+
+package com.Basic;
+
+public class A {
+    public void fun()
+    {
+    	System.out.println("Hello");
+    }
+}
+  class TEst{
+	public static void main(String[] args) {
+		A obj=new A();
+		
+		obj.fun();
+	}
+
+}
